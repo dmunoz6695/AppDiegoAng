59b73c3431492eba894922cef813b7cca9a465be
DM <difermu02@gmail.com>
2022-03-05 20:35:03 -0500

    Aregando Modulo theme y sus componentes

---
+++ src/app/theme/theme.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    SidenavComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ThemeModule { }
